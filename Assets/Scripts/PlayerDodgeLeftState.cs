dab4daeec420e4afa73d546185f6cc738bb1980c
kiennguyenGH <ktnguyen4647@gmail.com>
2023-10-02 17:17:51 -0700

    Adjust timer

--- Assets/Scripts/PlayerDodgeLeftState.cs
+++ Assets/Scripts/PlayerDodgeLeftState.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerDodgeLeftState : PlayerBaseState
 {
     float currentTime = 0f;
-    float startingTime = 2f;
+    float startingTime = 1f;
     public override void EnterState(PlayerStateManager player)
     {
         Debug.Log("Dodge Left");
@@ -12,7 +12,6 @@ public class PlayerDodgeLeftState : PlayerBaseState
 
     public override void UpdateState(PlayerStateManager player)
     {
-        Debug.Log(currentTime);
         currentTime -= 1 * Time.deltaTime;
         if (currentTime <= 0)
         {
