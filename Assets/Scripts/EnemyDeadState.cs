b9166276e667ba0c1ed9e11371e64dae69c0f4fc
alison <tmrfandom@gmail.com>
2023-12-13 14:27:38 -0800

    added transition upon enemy death

--- Assets/Scripts/EnemyDeadState.cs
+++ Assets/Scripts/EnemyDeadState.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class EnemyDeadState : EnemyBaseState
 {
+    GameObject panel;
+    Phase2TransitionAnimation phase2anim;
+ 
+    
     public override void EnterState(EnemyStateManager enemy)
     {
         Debug.Log("Enemy dead");
+        GameObject canvas = GameObject.Find("Canvas");
+        phase2anim = canvas.GetComponent<Phase2TransitionAnimation>();
+        phase2anim.AnimateOut();
     }
 
     public override void OnTriggerEnter2D(EnemyStateManager enemy, Collider2D collider)
