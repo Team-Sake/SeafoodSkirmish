b03da109620f54cbf9ec5e6a99da00a8cfe91784
Erika <erika.jasmine06@gmail.com>
2023-12-13 20:49:43 -0800

    recommitting phase 1 changes

--- Assets/NextLevelDifficulty.cs
+++ Assets/NextLevelDifficulty.cs
@@ -6,14 +6,14 @@ public class NextLevelDifficulty : MonoBehaviour
 {
     public void changeDifficulty()
     {
-        if(DifficultyLevel.difficulty == 1)
-        {
-            DifficultyLevel.difficulty = 2;
-        }
-        else if (DifficultyLevel.difficulty == 2)
-        {
-            DifficultyLevel.difficulty = 3;
-        }
+        //if(DifficultyLevel.difficulty == 1)
+        //{
+        //    DifficultyLevel.difficulty = 2;
+        //}
+       // else if (DifficultyLevel.difficulty == 2)
+       // {
+        //    DifficultyLevel.difficulty = 3;
+        //}
         //else if (DifficultyLevel.difficulty == 3)
         //{
           //  DifficultyLevel.difficulty = 1; 
