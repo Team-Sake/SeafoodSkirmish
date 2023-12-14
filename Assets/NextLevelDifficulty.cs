c0cf2b201d9c346ef3cbd34688dbb639abba4f46
Scott Lee <scottlee3447@gmail.com>
2023-12-13 17:01:30 -0800

    implemented difficulty levels

---
+++ Assets/NextLevelDifficulty.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevelDifficulty : MonoBehaviour
+{
+    public void changeDifficulty()
+    {
+        if(DifficultyLevel.difficulty == 1)
+        {
+            DifficultyLevel.difficulty = 2;
+        }
+        else if (DifficultyLevel.difficulty == 2)
+        {
+            DifficultyLevel.difficulty = 3;
+        }
+        else if (DifficultyLevel.difficulty == 3)
+        {
+            DifficultyLevel.difficulty = 1; 
+        }
+    }
+}
