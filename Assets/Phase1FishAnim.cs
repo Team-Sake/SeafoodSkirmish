c8955504e6c398a32a9f1cc4f767709bdff88faf
Scott Lee <scottlee3447@gmail.com>
2023-12-13 23:54:58 -0800

    updated phase 1 fish sprites depending on level

--- Assets/Phase1FishAnim.cs
+++ Assets/Phase1FishAnim.cs
@@ -1,13 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Image = UnityEngine.UI.Image;
 
 public class Phase1FishAnim : MonoBehaviour
 {
+    public Sprite salmon;
+    public Sprite shark;
+
     Animator animator;
+
+    Image image;
+
     void Start()
     {
         animator = transform.GetComponent<Animator>();
+        image = this.gameObject.GetComponent<Image>();
+
+        if(DifficultyLevel.difficulty == 2)
+        {
+            image.sprite = salmon;
+        }
+        else if(DifficultyLevel.difficulty == 3)
+        {
+            image.sprite = shark;
+        }
+        
     }
     public IEnumerator AnimateFish()
     {
